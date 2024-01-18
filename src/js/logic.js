551a32fecb477200e3e9d1f32955c5c323976434
Marek <ciesla.marek@gmail.com>
2024-01-19 00:28:53 +0100

    added import from date-fns | added function differenceInDays

--- src/js/logic.js
+++ src/js/logic.js
@@ -1,9 +1,10 @@
 import tax, { calculate, myFavouriteColor } from "./calculate.js";
+import differenceInDays from "date-fns";
 
-//   calculate(1);
+  calculate(1);
 
- const result = calculate(2);
- console.log(result)
+ const result2 = calculate(2);
+ console.log(result2)
 
 console.log(myFavouriteColor)
 
@@ -23,3 +24,17 @@ const clickFunction = () => {
 myButton.addEventListener('click', (e) => {
 const name = document.querySelector('.card__name');
 name.innerHTML = "KLIK KLIK"})
+alert('before');
+
+try {
+    const result = differenceInDays(
+        new Date(2020, 5, 1),
+        new Date(2020, 2, 1)
+      )
+} catch(e) {
+    console.log(e);
+}
+
+alert('OK');
+console.log(result)
+console.log('the same !!!')
